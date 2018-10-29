40c48d2e1b2de92c800723e24e15e03e30393179
Lyu <lyu@Lyus-MacBook-Pro.local>
2018-10-28 19:55:55 -0700

    Trending videos in different categories and time

---
+++ Category_Time.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from datetime import datetime as dt
+from matplotlib import pyplot as plt
+from matplotlib import dates as mdt
+import collections as cl
+
+csv = pd.read_csv('DEvideos.csv')
+
+df = pd.DataFrame(csv)
+
+# print(df.head())
+type = list(df.category_id)
+time = [x[0:10] for x in list(df.trending_date)]
+sz = len(type)
+cnt = cl.OrderedDict()
+for i in range(sz):
+    a = dt.strptime('20' + time[i], '%Y.%d.%m').date()
+    b = type[i]
+    if a not in cnt:
+        cnt[a] = [0]*50
+        cnt[a][b] = 1
+    else:
+        cnt[a][b] += 1
+
+xs = [d for d in list(cnt.keys())]
+
+ys = [x[1] for x in list(cnt.values())]
+
+plt.plot(xs, ys)
+plt.gcf().autofmt_xdate()
+plt.show()
+
+
+
