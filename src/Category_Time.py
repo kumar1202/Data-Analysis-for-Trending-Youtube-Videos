7ba6cd1cc4cd0f057cb92c48853682cb68830d6c
Lyu <jialun13131@hotmail.com>
2018-10-30 15:35:12 -0700

    Draw the fluctuation of all categories from DE

--- src/Category_Time.py
+++ src/Category_Time.py
@@ -1,21 +1,27 @@
 import pandas as pd
 from datetime import datetime as dt
 from matplotlib import pyplot as plt
-from matplotlib import dates as mdt
+import numpy as np
 import collections as cl
 
 csv = pd.read_csv('../datasets/DEvideos.csv')
+df_DE = pd.DataFrame(csv)
+# csv = pd.read_csv('../datasets/CAvideos.csv')
+# df_CA = pd.DataFrame(csv)
+# csv = pd.read_csv('../datasets/FRvideos.csv')
+# df_FR = pd.DataFrame(csv)
+# csv = pd.read_csv('../datasets/GBvideos.csv')
+# df_GB = pd.DataFrame(csv)
+# csv = pd.read_csv('../datasets/USvideos.csv')
+# df_US = pd.DataFrame(csv)
 
-df = pd.DataFrame(csv)
-
-# print(df.head())
-type = list(df.category_id)
-time = [x[0:10] for x in list(df.trending_date)]
-sz = len(type)
+cat = list(df_DE.category_id)
+time = [x[0:10] for x in list(df_DE.trending_date)]
+sz = len(cat)
 cnt = cl.OrderedDict()
 for i in range(sz):
     a = dt.strptime('20' + time[i], '%Y.%d.%m').date()
-    b = type[i]
+    b = cat[i]
     if a not in cnt:
         cnt[a] = [0]*50
         cnt[a][b] = 1
@@ -23,10 +29,21 @@ for i in range(sz):
         cnt[a][b] += 1
 
 xs = [d for d in list(cnt.keys())]
+for i in range(50):
+    ys = [x[i] for x in list(cnt.values())]
+    if sum(ys)//50 > 20:
+        plt.plot(xs, ys, label = str(i))
 
-ys = [x[1] for x in list(cnt.values())]
+# xs = ['201711', '201712', '201801', '201802', '201803', '201804',\
+#       '201805', '201806']
+# btm = np.array([0]*8)
+# for i in range(50):
+#     ys = np.array([x[i] for x in list(cnt.values())])
+#     if sum(ys)//50 > 20:
+#         plt.bar(xs, ys, bottom = btm, label = str(i))
+#         btm += ys
 
-plt.plot(xs, ys)
+plt.legend()
 plt.gcf().autofmt_xdate()
 plt.show()
 
