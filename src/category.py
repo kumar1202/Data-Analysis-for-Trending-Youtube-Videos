9458d9aad70f3a8875c7e726ca4526272ba3a9fe
Lin Ying Cheng <lychengr3x@gmail.com>
2018-11-09 11:10:07 -0800

    Rename variables and functions. Add new function.

--- src/category.py
+++ src/category.py
@@ -2,25 +2,30 @@ import json
 from file import *
 
 # load file paths
-fname = all_json()
-category_dict = {}
-for file in fname:
+_fname = all_json()
+_category_dict = {}
+for file in _fname:
     with open(file) as f:
         data = json.load(f)
         for i in range(len(data['items'])):
             channel_id = data['items'][i]['id']
             channel_cat = data['items'][i]['snippet']['title']
-            if channel_id not in category_dict:
-                category_dict.update({channel_id: channel_cat})
+            if channel_id not in _category_dict:
+                _category_dict.update({channel_id: channel_cat})
 
+def category_dict():
+    '''
+    :rtype: dict
+    Get a dictionary of id corresponding to its name.
+    '''
+    return _category_dict
 
-def cat_name(id):
+def category_name(id):
     '''
     :id: int
     ;rtype: str
     Get category name with given number `id`.
     '''
     assert isinstance(id, int)
-    return category_dict[str(id)]
+    return _category_dict[str(id)]
 
-# print(cat_name(23)) # Comedy
