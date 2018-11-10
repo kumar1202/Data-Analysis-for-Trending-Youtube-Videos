0a24d2c5bf644b7f4ffb5697beea9f9742ddb415
Lyu <jialun13131@hotmail.com>
2018-11-09 16:07:52 -0800

    Turn to draw the line graph

--- src/Category_Time.py
+++ src/Category_Time.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import collections as cl
 from datetime import datetime as dt
 from matplotlib import pyplot as plt
 import seaborn as sns
@@ -15,40 +16,11 @@ df_DE = pd.DataFrame(csv)
 # df_US = pd.DataFrame(csv)
 
 # print(df_DE.publish_time.head())
-p_date = [dt.strptime(x[0:10], '%Y-%m-%d').date() for x in list(df_DE.publish_time)]
 p_time = [int(x[11:13]) for x in list(df_DE.publish_time)]
-
-cnt_weekday = [0]*7
-cnt_time = [0]*24
-
-for i in range(len(p_date)):
-    cnt_weekday[p_date[i].weekday()]+=1
-    cnt_time[p_time[i]]+=1
-
-str_weekday = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-sns.barplot(x = 'Weekdays', y = 'Numbers', data=cnt_weekday)
-# plt.subplot(2, 1, 1)
-# plt.bar(str_weekday, cnt_weekday)
-# plt.subplot(2, 1, 2)
-# plt.bar(range(1, 25), cnt_time)
+mp = [0]*24
+for i in p_time:
+    mp[i]+=1
+plt.plot(range(1,25),mp, color = '#539caf', label='DE')
+plt.legend(loc='upper left')
 plt.show()
-# xs = [d for d in list(cnt.keys())]
-# for i in range(50):
-#     ys = [x[i] for x in list(cnt.values())]
-#     if sum(ys)//50 > 20:
-#         plt.plot(xs, ys, label = str(i))
-
-
-# btm = np.array([0]*8)
-# for i in range(50):
-#     ys = np.array([x[i] for x in list(cnt.values())])
-#     if sum(ys)//50 > 20:
-#         plt.bar(xs, ys, bottom = btm, label = str(i))
-#         btm += ys
-
-# plt.legend()
-# plt.gcf().autofmt_xdate()
-# plt.show()
-
-
 
