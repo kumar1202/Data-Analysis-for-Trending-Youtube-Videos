5eecac871eebf98ac0072811096d2f9c772ecab0
Lin Ying Cheng <lychengr3x@gmail.com>
2018-11-14 00:50:33 -0800

    Plot area views and total views in one figure.

--- src/views.py
+++ src/views.py
@@ -3,18 +3,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from category import *
 
+
 result = pd.DataFrame()
 for area in AREA:
     # read csv file
     df = pd.read_csv(csv_at(area))
 
     # get specific column
-    v_df = df[['category_id', 'views']]
+    v_df = pd.DataFrame(df[['category_id', 'views']])
 
     # Combine rows with same category_id
     v_df = v_df.groupby('category_id')['views'].sum().rename('area_views').\
         reset_index().sort_values(by='area_views', ascending=False)
-    v_df['area'] = area
+    v_df['area'] = AREA_FULL_NAME[area]
     result = result.append(v_df)
 
 # Add one new column: `category_name`
@@ -26,14 +27,16 @@ result['total_views'] = result.groupby(
     'category_id')['area_views'].transform('sum')
 
 # Sort total views in descending order.
-result = result.sort_values(by='total_views', ascending=False)
-# print(result)
+result = result.sort_values(by='total_views', ascending=False).reset_index(drop=True).\
+    reset_index().rename(index=str, columns={
+        'category_name': 'Categories', 'area_views': 'Views in Area', 'total_views': 'Total Views', 'area': 'Areas'})
 
-# # plot total views
-# sns.barplot(x="category_name", y="total_views", data=result,
-#             label="Total", color="b")
+# Plot total views.
+fig_total = sns.catplot(x="Categories", y="Total Views",
+                        data=result.head(50), kind='point')
 
-# plot area views
-sns.catplot(x="category_name", y="area_views", hue="area",
-            data=result, kind="bar", palette="muted")
+# Plot area views.
+fig_area = sns.catplot(x="Categories", y="Views in Area", hue="Areas",
+                       data=result.head(50), kind="bar", palette="muted", legend=False, ax=fig_total.ax.twinx())
+
 plt.show()
