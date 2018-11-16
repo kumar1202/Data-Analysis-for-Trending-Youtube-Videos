4c912bb939806b69c150456806d166497d683220
Lin Ying Cheng <lychengr3x@gmail.com>
2018-11-16 15:50:47 -0800

    Rename variables.

--- src/views.py
+++ src/views.py
@@ -4,40 +4,40 @@ import seaborn as sns
 from category import *
 
 
-result = pd.DataFrame()
+view_mix = pd.DataFrame()
 for area in AREA:
     # read csv file and get specific column
     # Combine rows with same category_id
-    df = pd.DataFrame(pd.read_csv(csv_at(area))[['category_id', 'views']]).groupby('category_id')[
+    df_view = pd.DataFrame(pd.read_csv(csv_at(area))[['category_id', 'views']]).groupby('category_id')[
         'views'].sum().rename('Views in Area').reset_index()
-    df['Areas'] = AREA_FULL_NAME[area]
-    result = result.append(df)
+    df_view['Areas'] = AREA_FULL_NAME[area]
+    view_mix = view_mix.append(df_view)
 
 # Add one new column: `Categories`
-result.insert(loc=1, column='Categories',
-              value=result.category_id.map(lambda x: category_name(x)))
+view_mix.insert(loc=1, column='Categories',
+              value=view_mix.category_id.map(lambda x: category_name(x)))
 
 # Add one new column `Total Views` of 5 areas.
-result['Total Views'] = result.groupby(
+view_mix['Total Views'] = view_mix.groupby(
     'category_id')['Views in Area'].transform('sum')
 
 # Sort total views in descending order.
-result = result.sort_values(
+view_mix = view_mix.sort_values(
     by='Total Views', ascending=False).reset_index(drop=True).reset_index()
-print(result.head(50))
+print(view_mix.head(50))
 
 # Increase font size
 sns.set(font_scale = 2)
 fig1, ax1 = plt.subplots()
 
 # Plot area views at ax1.
-fig_area = sns.catplot(x="Categories", y="Views in Area", data=result.head(35), hue="Areas",
+fig_area = sns.catplot(x="Categories", y="Views in Area", data=view_mix.head(35), hue="Areas",
                        hue_order=['Canada', 'France', 'Germany', 'Great British', 'USA'],
                        kind="bar", palette="muted", edgecolor="1", alpha=0.85, legend_out=False, ax=ax1)
 
 # Plot total views at ax2 which is twin ax of ax1.
 ax2 = ax1.twinx()
-fig_total = sns.catplot(x="Categories", y="Total Views", data=result.head(35),
+fig_total = sns.catplot(x="Categories", y="Total Views", data=view_mix.head(35),
                         kind='point', color="b", ax=ax2)
 
 # set figure
